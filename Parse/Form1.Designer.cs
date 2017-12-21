84ea19f3b289aaac7830995c13e8f246431075d4
huain.diario <huain.diario@AL4922YV>
2017-12-21 09:04:20 -0500

    Prueba GIT

---
+++ Parse/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Parse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Archivo Origen:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Directorio Destino:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(327, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(313, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(496, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Archivo Origen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(496, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Directorio Destino";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(210, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Parsear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(496, 158);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(390, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 252);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Parse Forti Log";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Parse/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+// esto
+
+
+namespace Parse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ArchivoOrigen = new OpenFileDialog();
+            ArchivoOrigen.Filter = "Archivo log|*.log";
+
+            DialogResult Resultado = ArchivoOrigen.ShowDialog();
+            if (Resultado == DialogResult.OK)
+            {
+                textBox1.Text = ArchivoOrigen.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog CarpetaDestino = new FolderBrowserDialog();
+            CarpetaDestino.ShowNewFolderButton = true;
+            if (CarpetaDestino.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = CarpetaDestino.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        private static string RemoveUnquotedWhiteSpaces(string text)
+        {
+            string result = String.Empty;
+            var parts = text.Split('"');
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (i % 2 == 0) result += Regex.Replace(parts[i], " ", "%");
+                else result += String.Format("\"{0}\"", parts[i]);
+            }
+            return result;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            String Linea;
+           
+            List<String> Campos = new List<String>();
+            Dictionary<String, String> CamposDiccionario = new Dictionary<string, string>();
+            int cuenta = 0;
+            char delimitador = '%';
+            String[] campo_valor;
+            String registro = "";
+            string archivoAParsear = textBox1.Text;
+            string detalle = textBox2.Text + "\\detalle.txt";
+            string encabezado = textBox2.Text + "\\encabezado.txt";
+            string archivoSalida = textBox2.Text + "\\Parseado_" + Path.GetFileName(archivoAParsear);
+            
+            using (StreamWriter salida1 = new StreamWriter(encabezado))
+            using (StreamWriter salida2 = new StreamWriter(detalle))
+            using (StreamReader Archivo = File.OpenText(archivoAParsear))
+            {
+                backgroundWorker1.ReportProgress(cuenta, "Parseando columnas....");
+
+                while ((Linea = Archivo.ReadLine()) != null)
+                {
+                    Campos.Clear();
+
+                    //Crea la lista Campos a partir de la linea del archivo.  Cada linea es de la forma "campo1=valor1 campo2=valor2"
+                    //La funcion RemoveUnquotesWhiteSpaces elimina los espacios que estan FUERA de los campos encerrados en comillas
+                    Campos.AddRange(RemoveUnquotedWhiteSpaces(Linea).Split(delimitador));
+
+                    registro = "";
+                    foreach (string columna in Campos)
+                    {
+                        campo_valor = columna.Split('=');
+                        if (!CamposDiccionario.ContainsKey(campo_valor[0]))
+                        {
+                            CamposDiccionario.Add(campo_valor[0], campo_valor[1]);
+                        }
+                        else
+                        {
+                            CamposDiccionario[campo_valor[0]] = campo_valor[1];
+                        }
+
+                    }
+
+                    foreach (var item in CamposDiccionario)
+                    {
+                        registro = registro + item.Value + ",";
+                    }
+                    registro = registro.TrimEnd(',');
+                    salida2.WriteLine(registro);
+
+                    cuenta++;
+                    backgroundWorker1.ReportProgress(cuenta);
+
+                    List<string> claves = new List<string>(CamposDiccionario.Keys);
+                    for (int i = 0; i < claves.Count(); i++)
+                    {
+                        string clave = claves[i];
+                        CamposDiccionario[clave] = "";
+                    }
+
+
+                }
+                registro = "";
+                foreach (var item in CamposDiccionario)
+                {
+                    registro = registro + item.Key + ",";
+                }
+                registro = registro.TrimEnd(',');
+                salida1.WriteLine(registro);
+
+
+                salida1.Close();
+                salida2.Close();
+                salida1.Dispose();
+                salida2.Dispose();
+                Archivo.Close();
+                Archivo.Dispose();
+
+
+            }
+
+            File.Copy(encabezado, archivoSalida);
+
+            using (StreamWriter salida = new StreamWriter(archivoSalida, append:true))
+            using (StreamReader entrada = new StreamReader(detalle))
+            {
+                while ((Linea = entrada.ReadLine()) != null)
+                {
+                    salida.WriteLine(Linea);
+                }
+
+                salida.Close();
+                entrada.Close();
+                salida.Dispose();
+                entrada.Dispose();
+
+            }
+
+
+            File.Delete(encabezado);
+            File.Delete(detalle);
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.UserState != null)
+            {
+                label3.Text = e.UserState.ToString();
+            }
+
+            textBox3.Text = e.ProgressPercentage.ToString();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+    }
+}
